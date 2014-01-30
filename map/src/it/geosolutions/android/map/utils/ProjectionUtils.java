c28c7955370611e72e1027f37c30fce2aee3f1ab
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2014-01-30 13:58:36 +0100

    fixed problems in projection limits

--- map/src/it/geosolutions/android/map/utils/ProjectionUtils.java
+++ map/src/it/geosolutions/android/map/utils/ProjectionUtils.java
@@ -1,21 +1,39 @@
+/*
+ * GeoSolutions - MapstoreMobile - GeoSpatial Framework on Android based devices
+ * Copyright (C) 2014  GeoSolutions (www.geo-solutions.it)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.geosolutions.android.map.utils;
 
-import org.mapsforge.android.maps.MapScaleBar;
 import org.mapsforge.core.util.MercatorProjection;
 
 /**
  * Utility Class to convert WebMercator and Geographic coordinates
  * Waiting for integration of a mor complex library to do that 
  * CHECK THIS CLASS results of toGeographicX and Y is incorrect!
- * @author Admin
+ * @author Lorenzo Natali (lorenzo.natali@geo-solutions.it
  *
  */
 public class ProjectionUtils {
-	public static double toGeographicX( double mercatorX_lon){
-		//TODO optimize 
-	    if (Math.abs(mercatorX_lon) > 180)
-	        throw new IllegalArgumentException();
-
+	
+	/**
+	 * Convert a EPSG:900913 longitude (meeters) into geographic WGS:84 (degrees)
+	 * @param mercatorX_lon the longitude  in Mercator projection
+	 * @return the longitude in WGS84
+	 */
+	public static double toGeographicX( double mercatorX_lon){ 
 	    if ((Math.abs(mercatorX_lon) > MercatorProjection.EARTH_CIRCUMFERENCE/2) )
 	    	throw new IllegalArgumentException();
 
@@ -26,12 +44,13 @@ public class ProjectionUtils {
 	    return num4 - (num5 * 360.0);
 	 	    
 	}
-	public static double toGeographicY( double mercatorY_lat)
-	{
-		//TODO optimize 
-	    if (Math.abs(mercatorY_lat) > 90){
-	    	 throw new IllegalArgumentException();
-	    }
+	
+	/**
+	 * Convert a EPSG:900913 latitude (meeters) into geographic EPSG:4326 (degrees)
+	 * @param mercatorY_lat the latitude  in Mercator projection
+	 * @return the latitude in WGS84
+	 */
+	public static double toGeographicY( double mercatorY_lat){
 	    if ((Math.abs(mercatorY_lat) >  MercatorProjection.EARTH_CIRCUMFERENCE/2) ){
 	    	 throw new IllegalArgumentException();
 	    }
@@ -41,23 +60,31 @@ public class ProjectionUtils {
 	    return num7 * 57.295779513082323;
 	}
 
-	public static double  toWebMercatorX( double mercatorX_lon)
-	{
+	/**
+	 * Convert a  geographic longitude WGS84 (degrees) into EPSG:900913 latitude (meeters)
+	 * @param lon the longitude to convert
+	 * @return longitude in meeters (EPSG:900913)
+	 */
+	public static double  toWebMercatorX( double lon){
 		//TODO optimize 
-	    if (Math.abs(mercatorX_lon) > 180){
+	    if (Math.abs(lon) > 180 ){
 	    	throw new IllegalArgumentException();
 	    }
-	    double num = mercatorX_lon * 0.017453292519943295;
+	    double num = lon * 0.017453292519943295;
 	    return 6378137.0 * num;
 	}
 	
-	public static double toWebMercatorY(   double mercatorY_lat)
-	{
+	/**
+	 * Convert a  geographic latitude WGS84 (degrees) into EPSG:900913 latitude (meeters)
+	 * @param lat latitude to convert
+	 * @return latitude in meeters (EPSG:900913)
+	 */
+	public static double toWebMercatorY(double lat)	{
 		//TODO optimize 
-	    if ( Math.abs(mercatorY_lat) > 90){
+	    if ( Math.abs(lat) > 90){
 	    	throw new IllegalArgumentException();
 	    }
-	    double a = mercatorY_lat * 0.017453292519943295;
+	    double a = lat * 0.017453292519943295;
 	    return 3189068.5 * Math.log((1.0 + Math.sin(a)) / (1.0 - Math.sin(a)));
 	}
 }
