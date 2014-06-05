d4fd3d541daae6c77fd8cb01a425a834220bbbc0
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2014-06-05 14:42:50 +0200

    fixed projection problems with marker overlay when viewport near projection limit

--- map/src/it/geosolutions/android/map/overlay/MarkerOverlay.java
+++ map/src/it/geosolutions/android/map/overlay/MarkerOverlay.java
@@ -22,14 +22,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.geosolutions.android.map.overlay.items.DescribedMarker;
+import it.geosolutions.android.map.utils.ProjectionUtils;
 
 import org.mapsforge.android.maps.MapViewPosition;
 import org.mapsforge.android.maps.Projection;
 import org.mapsforge.android.maps.overlay.ListOverlay;
 import org.mapsforge.android.maps.overlay.OverlayItem;
+import org.mapsforge.core.model.BoundingBox;
+import org.mapsforge.core.model.GeoPoint;
+import org.mapsforge.core.model.Point;
+import org.mapsforge.core.util.MercatorProjection;
+
+import android.graphics.Canvas;
 
 public class MarkerOverlay extends ListOverlay {
+	Projection projection;
 	
+	public Projection getProjection() {
+		return projection;
+	}
+
+	public void setProjection(Projection projection) {
+		this.projection = projection;
+	}
+
 	/**
 	 * Query a pixel. Returns a marker drawn in that pixel
 	 * Used to select/drag markers
@@ -40,6 +56,8 @@ public class MarkerOverlay extends ListOverlay {
 	 * @return
 	 */
 	public DescribedMarker queryPixel(MapViewPosition mapViewPosition,Projection projection,float x, float y) {
+		
+		
 		
 		for(OverlayItem i : getOverlayItems()){
 			//TODO use architectural instrument to get markers
@@ -77,6 +95,40 @@ public class MarkerOverlay extends ListOverlay {
 			}
 		}
 	}
+	
+	@Override
+    public synchronized void draw(BoundingBox boundingBox, byte zoomLevel, Canvas canvas) {
+			double canvasPixelLeft,canvasPixelTop;
+			if(projection == null){
+				 canvasPixelLeft = MercatorProjection.longitudeToPixelX(boundingBox.minLongitude, zoomLevel);
+		         canvasPixelTop = MercatorProjection.latitudeToPixelY(boundingBox.maxLatitude, zoomLevel);
+			}else{
+				double n = boundingBox.maxLatitude;
+	    		double w = boundingBox.minLongitude;
+	    		double s = boundingBox.minLatitude;
+	    		double e = boundingBox.maxLongitude;
+	    		GeoPoint dp = new GeoPoint(n, w); // ULC
+	    		long[] pxDp= ProjectionUtils.getDrawPoint(dp, projection, zoomLevel);
+	            canvasPixelLeft = pxDp[0];
+	    		canvasPixelTop= pxDp[1];
+			}
+		
+           
+            
+
+    		// replaces the argument
+    		
+            Point canvasPosition = new Point(canvasPixelLeft, canvasPixelTop);
+
+            synchronized (this.getOverlayItems()) {
+                    int numberOfOverlayItems = this.getOverlayItems().size();
+                    for (int i = 0; i < numberOfOverlayItems; ++i) {
+                            OverlayItem overlayItem = this.getOverlayItems().get(i);
+                            overlayItem.draw(boundingBox, zoomLevel, canvas, canvasPosition);
+                    }
+            }
+    }
+
 
 	
 	
