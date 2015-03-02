83747164cfa28dbe482c24617b5ca6c75679b007
Lorenzo Pini <lore.pini@tin.it>
2015-03-02 15:10:39 +0100

    Use a default icon for ActionBar

--- geocollect/android/core/src/it/geosolutions/geocollect/android/core/form/utils/FormUtils.java
+++ geocollect/android/core/src/it/geosolutions/geocollect/android/core/form/utils/FormUtils.java
@@ -40,242 +40,250 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.util.Log;
 
-
 /**
  * A static class that provide utility methods
+ * 
  * @author Lorenzo Natali (lorenzo.natali@geo-solutions.it)
- *
+ * 
  */
 public class FormUtils {
-	private static String ICON_SAVE ="ic_save";
-	private static String ICON_SEND ="ic_send";
-	private static String ICON_ACCEPT="ic_accept";
-	private static String ICON_CAMERA="ic_camera";
-	private static String ICON_CENTER_ON_MARKER="ic_center";
-	private static String ICON_LOCALIZE="ic_localize";
+    private static String ICON_SAVE = "ic_save";
 
-	
-	
-	/**
-	 * Provides the drawable for the icon class in <MissionTemplate>
-	 * @param iconCls
-	 * @return
-	 */
-	public static Integer getDrawable(String iconCls) {
-		if(iconCls == null){
-			return null;
-		}
-		if(ICON_SAVE.equals(iconCls)){			
-			return R.drawable.ic_navigation_accept;
-		}else if(ICON_SEND.equals(iconCls)){
-			return R.drawable.ic_social_send_now;
-		}else if(ICON_ACCEPT.equals(iconCls)){
-			return R.drawable.ic_rating_good;
-		}else if(ICON_CAMERA.equals(iconCls)){
-			return R.drawable.ic_device_access_camera;
-		}else if(ICON_CENTER_ON_MARKER.equals(iconCls)){
-			return R.drawable.ic_center;
-		}else if(ICON_LOCALIZE.equals(iconCls)){
-			return R.drawable.ic_localize_marker;
-		}else{
-			return null;
-		}
-	}
+    private static String ICON_SEND = "ic_send";
 
+    private static String ICON_ACCEPT = "ic_accept";
 
+    private static String ICON_CAMERA = "ic_camera";
 
-	/**
-	 * @param a
-	 */
-	public static AndroidAction getAndroidAction(FormAction a) {
-		switch(a.type){
-		case confirm:
-		case send:
-			return new SendAction(a);
-		case save:
-			return new SaveAction(a);
-		case photo:
-			return new CameraAction(a);
-		case video:
-			break;
-		case localize:
-			return new LocalizeAction(a);
-		case center:
-			return new CenterOnMarkerAction(a);
-		}
-		return null;
-		
-	}
-	
-	/**
-	 * resize the images in the folder to the provided max size of the larger dimension
-	 * @param feature_id
-	 * @param maxSize
-	 */
-	public static void resizeFotosToMax(final Context context,final String feature_id,final int maxSize){
-		
-		if(feature_id == null || feature_id.isEmpty()){
-			Log.w("FormUtils", "getPhotoUriStrings: Could not get feature_id");
-			return;
-		}
-			
-		File folder = new File(MapFilesProvider.getEnvironmentDirPath(context)+"/geocollect/media/"+feature_id);
-		folder.mkdirs();
-		File[] listOfFiles = folder.listFiles();
+    private static String ICON_CENTER_ON_MARKER = "ic_center";
 
-		if(listOfFiles == null){
-			// Zero-length array as "not found"
-			return;
-		}
-		
-		//resize
-		
-		for (int i = 0; i < listOfFiles.length; i++) {
+    private static String ICON_LOCALIZE = "ic_localize";
 
-			Bitmap resizedBitmap = null;
-			try {
-				
-				//Following http://developer.android.com/training/displaying-bitmaps/load-bitmap.html#load-bitmap
-				//to avoid OOE decoding bitmaps, read out the bounds of the image, calculate the sample size
-				//and only then decode the already resized image, which will need less memory
-				
-				BitmapFactory.Options options = new BitmapFactory.Options();
-				//this won't allocate memory but read out only the bounds of the image
-				options.inJustDecodeBounds = true;
-				BitmapFactory.decodeFile(listOfFiles[i].getAbsolutePath(), options);
+    /**
+     * Provides the drawable for the icon class in <MissionTemplate> If the correct drawable cannot be found, returns a question mark icon
+     * 
+     * @param iconCls
+     * @return
+     */
+    public static Integer getDrawable(String iconCls) {
+        if (iconCls == null) {
+            return R.drawable.ic_action_help;
+        }
+        if (ICON_SAVE.equals(iconCls)) {
+            return R.drawable.ic_navigation_accept;
+        } else if (ICON_SEND.equals(iconCls)) {
+            return R.drawable.ic_social_send_now;
+        } else if (ICON_ACCEPT.equals(iconCls)) {
+            return R.drawable.ic_rating_good;
+        } else if (ICON_CAMERA.equals(iconCls)) {
+            return R.drawable.ic_device_access_camera;
+        } else if (ICON_CENTER_ON_MARKER.equals(iconCls)) {
+            return R.drawable.ic_center;
+        } else if (ICON_LOCALIZE.equals(iconCls)) {
+            return R.drawable.ic_localize_marker;
+        } else {
+            return R.drawable.ic_action_help;
+        }
+    }
 
-				// Calculate inSampleSize
-				final int sampleSize = calculateInSampleSize(options,maxSize);
-				if(sampleSize == 1){
-					//no need to resample, this image is small enough
-					continue;
-				}
-				options.inSampleSize = sampleSize;
+    /**
+     * @param a
+     */
+    public static AndroidAction getAndroidAction(FormAction a) {
+        switch (a.type) {
+        case confirm:
+        case send:
+            return new SendAction(a);
+        case save:
+            return new SaveAction(a);
+        case photo:
+            return new CameraAction(a);
+        case video:
+            break;
+        case localize:
+            return new LocalizeAction(a);
+        case center:
+            return new CenterOnMarkerAction(a);
+        }
+        return null;
 
-				// Decode bitmap with inSampleSize set
-				options.inJustDecodeBounds = false;
-				resizedBitmap = BitmapFactory.decodeFile(listOfFiles[i].getAbsolutePath(), options);
+    }
 
-			} catch (OutOfMemoryError e) {
-				//according to http://stackoverflow.com/questions/7138645/catching-outofmemoryerror-in-decoding-bitmap
-				//we could try to catch that OOE and do System.gc() but I think the above should avoid OOEs
-				//so for now give up
-				Log.e(FormUtils.class.getSimpleName(), "OOE decoding Bitmap ",e);
-				continue;
-			}
+    /**
+     * resize the images in the folder to the provided max size of the larger dimension
+     * 
+     * @param feature_id
+     * @param maxSize
+     */
+    public static void resizeFotosToMax(final Context context, final String feature_id,
+            final int maxSize) {
 
+        if (feature_id == null || feature_id.isEmpty()) {
+            Log.w("FormUtils", "getPhotoUriStrings: Could not get feature_id");
+            return;
+        }
 
-			try {
-				final File origFile = listOfFiles[i];
+        File folder = new File(MapFilesProvider.getEnvironmentDirPath(context)
+                + "/geocollect/media/" + feature_id);
+        folder.mkdirs();
+        File[] listOfFiles = folder.listFiles();
 
-				final String newfileName = origFile.getName().substring(0,origFile.getName().lastIndexOf("."))+"_resized";
-				final String extension   = origFile.getName().substring(origFile.getName().lastIndexOf(".") + 1);
+        if (listOfFiles == null) {
+            // Zero-length array as "not found"
+            return;
+        }
 
-				final String newFile = origFile.getPath().substring(0,origFile.getPath().lastIndexOf("/") + 1) + newfileName+"."+extension;
+        // resize
 
-				FileOutputStream fOut = new FileOutputStream(newFile);
+        for (int i = 0; i < listOfFiles.length; i++) {
 
-				resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            Bitmap resizedBitmap = null;
+            try {
 
-				fOut.flush();
-				fOut.close();
+                // Following http://developer.android.com/training/displaying-bitmaps/load-bitmap.html#load-bitmap
+                // to avoid OOE decoding bitmaps, read out the bounds of the image, calculate the sample size
+                // and only then decode the already resized image, which will need less memory
 
-				//all worked out, delete original
-				origFile.delete();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                // this won't allocate memory but read out only the bounds of the image
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(listOfFiles[i].getAbsolutePath(), options);
 
-			} catch (FileNotFoundException e) {
-				Log.e(FormUtils.class.getSimpleName(), e.getClass().getSimpleName(),e);
-			} catch (IOException e) {
-				Log.e(FormUtils.class.getSimpleName(), e.getClass().getSimpleName(),e);
-			}
+                // Calculate inSampleSize
+                final int sampleSize = calculateInSampleSize(options, maxSize);
+                if (sampleSize == 1) {
+                    // no need to resample, this image is small enough
+                    continue;
+                }
+                options.inSampleSize = sampleSize;
 
-		}
-	}
-	/**
-	 * calculates the samplesize (the scale factor 1 / n) to subsample the image
-	 * Note : A power of two value is calculated because the decoder uses a final value by rounding down
-	 * to the nearest power of two, as per the inSampleSize documentation.
-	 * @param options
-	 * @param maxSize
-	 * @return
-	 */
-	public static int calculateInSampleSize(BitmapFactory.Options options, final int maxSize) {
-		
-		// Raw height and width of image
-		final int height = options.outHeight;
-		final int width = options.outWidth;
-		int inSampleSize = 1;
+                // Decode bitmap with inSampleSize set
+                options.inJustDecodeBounds = false;
+                resizedBitmap = BitmapFactory.decodeFile(listOfFiles[i].getAbsolutePath(), options);
 
-		
-		int max = height > width ? height : width;
+            } catch (OutOfMemoryError e) {
+                // according to http://stackoverflow.com/questions/7138645/catching-outofmemoryerror-in-decoding-bitmap
+                // we could try to catch that OOE and do System.gc() but I think the above should avoid OOEs
+                // so for now give up
+                Log.e(FormUtils.class.getSimpleName(), "OOE decoding Bitmap ", e);
+                continue;
+            }
 
-		float factor = (float) max / maxSize;
+            try {
+                final File origFile = listOfFiles[i];
 
-		int reqWidth  = (int) (width / factor);
-		int reqHeight = (int) (height / factor);
-		
-		if (height > reqHeight || width > reqWidth) {
+                final String newfileName = origFile.getName().substring(0,
+                        origFile.getName().lastIndexOf("."))
+                        + "_resized";
+                final String extension = origFile.getName().substring(
+                        origFile.getName().lastIndexOf(".") + 1);
 
-			// Calculate the  inSampleSize value that is a power of 2 and will keep both
-			// height and width smaller than the required
-			while (height / inSampleSize > reqHeight && width / inSampleSize > reqWidth) {
-				inSampleSize *= 2;
-			}
-		}
+                final String newFile = origFile.getPath().substring(0,
+                        origFile.getPath().lastIndexOf("/") + 1)
+                        + newfileName + "." + extension;
 
-		return inSampleSize;
-	}
+                FileOutputStream fOut = new FileOutputStream(newFile);
 
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
 
+                fOut.flush();
+                fOut.close();
 
-	/**
-	 * Return a list of Strings representing Uris for the media
-	 * TODO: Filter media based on feature.id
-	 * @return
-	 */
-	public static String[] getPhotoUriStrings(final Context context, final String feature_id){
-		
-		if(feature_id == null || feature_id.isEmpty()){
-			Log.w("FormUtils", "getPhotoUriStrings: Could not get feature_id");
-			return new String[0];
-		}
-			
-		File folder = new File(MapFilesProvider.getEnvironmentDirPath(context)+"/geocollect/media/"+feature_id);//TODO Parametrize this
-		folder.mkdirs();
-		File[] listOfFiles = folder.listFiles();
+                // all worked out, delete original
+                origFile.delete();
 
-		if(listOfFiles == null){
-			// Zero-length array as "not found"
-			return new String[0];
-		}
-		
-		ArrayList<String> newFileListUri = new ArrayList<String>();
-	    for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				newFileListUri.add(Uri.fromFile(listOfFiles[i]).toString());
-			} 
-	    }
-	    
-	    return newFileListUri.toArray(new String[newFileListUri.size()]);
-	}
+            } catch (FileNotFoundException e) {
+                Log.e(FormUtils.class.getSimpleName(), e.getClass().getSimpleName(), e);
+            } catch (IOException e) {
+                Log.e(FormUtils.class.getSimpleName(), e.getClass().getSimpleName(), e);
+            }
 
+        }
+    }
 
-	/**
-	 * Given a string encoded file URI, delete that file
-	 * @param filePath
-	 * @return boolean whether the file was deleted or not
-	 */
-	public static boolean deleteFile(String filePath){
-		if(filePath == null)
-			return false;
-		
-		URI fileURI = URI.create(filePath);
-		
-		File f = new File(fileURI);
-		if(f == null || !f.exists() || !f.isFile()|| !f.canWrite() ){
-			return false;
-		}
-		return f.delete();
-		
-	}
+    /**
+     * calculates the samplesize (the scale factor 1 / n) to subsample the image Note : A power of two value is calculated because the decoder uses a
+     * final value by rounding down to the nearest power of two, as per the inSampleSize documentation.
+     * 
+     * @param options
+     * @param maxSize
+     * @return
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options, final int maxSize) {
+
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        int max = height > width ? height : width;
+
+        float factor = (float) max / maxSize;
+
+        int reqWidth = (int) (width / factor);
+        int reqHeight = (int) (height / factor);
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate the inSampleSize value that is a power of 2 and will keep both
+            // height and width smaller than the required
+            while (height / inSampleSize > reqHeight && width / inSampleSize > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    /**
+     * Return a list of Strings representing Uris for the media TODO: Filter media based on feature.id
+     * 
+     * @return
+     */
+    public static String[] getPhotoUriStrings(final Context context, final String feature_id) {
+
+        if (feature_id == null || feature_id.isEmpty()) {
+            Log.w("FormUtils", "getPhotoUriStrings: Could not get feature_id");
+            return new String[0];
+        }
+
+        File folder = new File(MapFilesProvider.getEnvironmentDirPath(context)
+                + "/geocollect/media/" + feature_id);// TODO Parametrize this
+        folder.mkdirs();
+        File[] listOfFiles = folder.listFiles();
+
+        if (listOfFiles == null) {
+            // Zero-length array as "not found"
+            return new String[0];
+        }
+
+        ArrayList<String> newFileListUri = new ArrayList<String>();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                newFileListUri.add(Uri.fromFile(listOfFiles[i]).toString());
+            }
+        }
+
+        return newFileListUri.toArray(new String[newFileListUri.size()]);
+    }
+
+    /**
+     * Given a string encoded file URI, delete that file
+     * 
+     * @param filePath
+     * @return boolean whether the file was deleted or not
+     */
+    public static boolean deleteFile(String filePath) {
+        if (filePath == null)
+            return false;
+
+        URI fileURI = URI.create(filePath);
+
+        File f = new File(fileURI);
+        if (f == null || !f.exists() || !f.isFile() || !f.canWrite()) {
+            return false;
+        }
+        return f.delete();
+
+    }
 }
